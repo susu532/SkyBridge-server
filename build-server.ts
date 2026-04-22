93ce0e727e49a95bae0acef079afcd529e3dd332
mimi489 <nnnrrrtttsss123@gmail.com>
2026-04-22 11:05:26 +0100

    update

--- build-server.ts
+++ build-server.ts
@@ -1,11 +1,11 @@
 import * as esbuild from 'esbuild';
 
 esbuild.build({
-  entryPoints: ['index.ts'],
+  entryPoints: ['server.ts'],
   bundle: true,
   platform: 'node',
   target: 'node18',
-  outfile: 'dist/index.cjs',
+  outfile: 'dist/server.cjs',
   format: 'cjs',
   external: ['express', 'socket.io', 'vite']
 }).catch(() => process.exit(1));
